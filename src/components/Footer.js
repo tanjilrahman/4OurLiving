767469fd783968afe9f53b18bf56688306ffed27
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2022-11-23 22:08:13 +0600

    added tribe logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,39 @@
 import logo from "../logo footer.svg";
+
 const Footer = () => {
   return (
     <div className="bg-black text-white">
-      <div className="max-w-7xl mx-auto text-center items-center space-y-8 py-12 text-xs md:text-sm lg:text-base">
+      <div className="max-w-7xl mx-auto text-center items-center py-12 text-xs md:text-sm lg:text-base">
         <img src={logo} alt="logo" className="mx-auto w-60 md:w-72 lg:w-auto" />
-        <p className="px-8 max-w-3xl text-center mx-auto">
+        <p className="px-8 max-w-3xl text-center mx-auto mt-2">
           Four Our Living can assist you at every step of the removal and
           disposal process. A door-to-door waste removal and recycling service
           for multi-family residential homes such as apartment buildings,
           jointly owned dwellings, retirement communities and student
           accommodation.
         </p>
+        <a
+          href="https://www.tribeninjas.com/"
+          className="hover:cursor-pointer hover:underline font-semibold"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="./Tribe-logo.png"
+            alt="TribeLogo"
+            className="mx-auto w-24 md:w-44 my-0"
+          />
+        </a>
+
         <p>
-          © 2022{" "}
+          © 2022 Website and Marketing by{" "}
           <a
             href="https://www.tribeninjas.com/"
-            className="hover:cursor-pointer hover:underline"
+            className="hover:cursor-pointer hover:underline font-semibold"
             target="_blank"
             rel="noopener noreferrer"
           >
-            TribeNinjas.
+            Tribe Ninjas
           </a>
         </p>
       </div>
